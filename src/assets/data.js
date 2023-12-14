3f4a494248085800cd53477967a77f30628f51d6
osamasayd <osamasayd141@gmail.com>
2023-12-15 00:27:57 +0300

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,27 @@
+export const CORE_CONCEPTS = {
+  Menu:{
+    title: "Menu",
+    description:
+      "The core UI building block - compose the user interface by combining multiple components.",
+  },
+  Home:{
+    title: "Home",
+    description:
+      "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered.",
+  },
+  ToDo:{
+    title: "ToDo",
+    description:
+      "Make components configurable (and therefore reusable) by passing input data to them.",
+  },
+  About:{
+    title: "About",
+    description:
+      "React-managed data which, when changed, causes the component to re-render & the UI to update.",
+  },
+  XO_App:{
+    title: "xo",
+    description:
+      "React-managed data which, when changed, causes the component to re-render & the UI to update.",
+  }
+};
