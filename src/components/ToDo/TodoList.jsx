9401468145937591b41c9f459bf8b5c259641ddb
osamasayd <osamasayd141@gmail.com>
2023-12-15 00:29:47 +0300

    set TodoList component

---
+++ src/components/ToDo/TodoList.jsx
@@ -0,0 +1,45 @@
+import './TodoList.css'
+import React, { useState } from 'react';
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <br/>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
