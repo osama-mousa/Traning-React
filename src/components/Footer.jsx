1c7deaafcd297d7b304bfe5a1838c6acea23f576
osamasayd <osamasayd141@gmail.com>
2023-12-15 00:29:00 +0300

    set Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <>
+            <div className="footer">
+                Copyright &copy; 2023 All Right Reserved To MOUSSA &reg; Team
+            </div>
+        </>
+    )
+}
+
+export default Footer;
